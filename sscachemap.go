2de7e26b3ca5be0911e3e594e615aac31b1aa628
chinaboard <chinaboard@gmail.com>
2017-11-27 17:50:50 +0800

    format code

--- sscachemap.go
+++ sscachemap.go
@@ -23,13 +23,10 @@ func (table *SSCacheMap) Set(key interface{}, value interface{}, lifeSpan time.D
 
 func (table *SSCacheMap) GetOrAdd(key interface{}, value interface{}, lifeSpan time.Duration) interface{} {
 	item := NewSSCacheItem(key, value, lifeSpan)
-
 	v, loaded := table.items.LoadOrStore(key, item)
-
 	if loaded {
 		v.(*SSCacheItem).createdOn = time.Now()
 	}
-
 	return v.(*SSCacheItem).value
 }
 
@@ -46,24 +43,19 @@ func (table *SSCacheMap) Delete(key interface{}) {
 }
 
 func (table *SSCacheMap) expirationCheck() {
-
 	for {
 		now := time.Now()
-
 		keys := make([]interface{}, 0)
 		table.items.Range(func(key, value interface{}) bool {
-
 			item := value.(*SSCacheItem)
 			if item.lifeSpan > 0 && now.Sub(item.createdOn) > item.lifeSpan {
 				keys = append(keys, key)
 			}
 			return true
 		})
-
 		for _, key := range keys {
 			table.items.Delete(key)
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
-
 }
